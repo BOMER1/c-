e16c112fc381394fd66967aec827376fc2e5ed6c
BOMER1 <anhdtth2010054@fpt.edu.vn>
2021-08-14 08:01:09 +0700

    btvn c#

---
+++ C--main/Demo/Wxample3/Wxample3/StatementDemo.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wxample3
+{
+    class StatementDemo
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+
+            Console.WriteLine("Nhập vào số thứ 1: ");
+            int a = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Nhập vào số thứ 2: ");
+            int b = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Nhập vào số thứ 3: ");
+            int c = int.Parse(Console.ReadLine());
+
+            int max;
+
+            /* cách 1
+            if (max < b)
+            {
+                max = b;
+            }
+
+            if (max < c)
+            {
+                max = c;
+            }
+            */
+
+            //cách 2
+            max = a > b ? a : b;
+            max = max > c ? max : c;
+
+
+            /*switch (max > b)
+            {
+                case true:
+                    switch (max > c)
+                    {
+                        case false:
+                            max = c;
+                            break;
+                    }
+                    break;
+                case false:
+                    max = b;
+                    break;
+            }*/
+
+            Console.WriteLine("Max trong 3 số {0}, {1}, {2} là {3}", a, b, c, max);
+
+            Console.ReadKey();
+        }
+    }
+    }
+
---
+++ C--main/Demo/Wxample3/Wxample3/StatementDemo2.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wxample3
+{
+    class StatementDemo2
+    {
+        static void Main(string[] agrs)
+        {
+            Console.WriteLine("Enter i,j,k: ");
+            int i = Convert.ToInt32(Console.ReadLine());
+            int j = Convert.ToInt32(Console.ReadLine());
+            int k = Convert.ToInt32(Console.ReadLine());
+            if ((i != j && j != k) || (i==j&&j!=k) ||(i!=j&&j==k) || (i==k&&j!=k))
+            {
+               //toan tu 3 ngoi
+              int  max = i > j ? i : j;   max = max > k ? max : k;
+                Console.WriteLine("Max la: {0}", max);
+            }
+            else
+            {
+                Console.WriteLine("3 so bang nhau.");
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
