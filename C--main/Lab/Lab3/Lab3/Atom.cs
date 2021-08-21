59b460704f6fabfc9f71477fa914d42716b978dd
BOMER1 <anhdtth2010054@fpt.edu.vn>
2021-08-21 08:13:12 +0700

    lab 2 and 3

---
+++ C--main/Lab/Lab3/Lab3/Atom.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3
+{
+    class Atom
+    {
+        public int atomicNumber;
+        public string atomicSymbol;
+        public string atomicName;
+        public double atomicWeight;
+
+        public Atom()
+        {
+            atomicNumber = atomicNumber;
+            atomicSymbol = atomicSymbol;
+            atomicName = atomicName;
+            atomicWeight = atomicWeight;
+        }
+        
+
+
+        public virtual void Display()
+        {
+            Console.WriteLine("{0}  {1}  {2}  {3}", atomicNumber, atomicSymbol, atomicName, atomicWeight);
+        }
+
+    }
+}
